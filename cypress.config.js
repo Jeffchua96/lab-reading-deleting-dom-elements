e37e68198dcf9dd0c1f5bd19e73dcae962b2c0fa
Jeffchua96 <jeffersonchua@pursuit.org>
2023-03-19 12:53:35 -0400

    lab reading deleting

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,20 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  'cypress-watch-and-reload': {
-    watch: ['index.html', 'main.js', 'style.css'],
+  "cypress-watch-and-reload": {
+    watch: ["index.html", "main.js", "style.css"],
   },
-})
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "webpack",
+    },
+  },
+});
--- main.js
+++ main.js
@@ -13,3 +13,43 @@ document.querySelectorAll(".posts h3")
 .forEach(el => {
     el.remove()
 })
+
+let firstSection = document.querySelector("section");
+firstSection.classList.add("featured");
+
+let article = document.createElement("article");
+let image = document.createElement("img");
+image.src = "./images/paul-gilmore-unsplash.jpg";
+image.alt = "Image of a mountain in front of a lake.";
+article.append(image);
+
+let h3 = document.createElement("h3");
+h3.textContent = "Stop Planning";
+article.append(h3);
+
+let paragraph = document.createElement("p");
+paragraph.textContent = "You -- yes you! You're an over-planner, I can tell. It's time to stop planning so much and instead focusing on relaxing. Taking a break at all is so stressful these days; why add to the stress by overworking yourself?";article.append(paragraph);
+
+let aside = document.createElement("aside");
+article.append(aside);
+let asideText = document.createElement("p");
+aside.append(asideText);
+let span = document.createElement("span");
+asideText.append(span);
+let strong = document.createElement("strong");
+strong.textContent = "Read Time:";
+span.append(strong);
+span.innerHTML += " 4 Minutes ";
+let link = document.createElement("a");
+link.href = "#";link.textContent = "Read more...";
+asideText.append(link);
+
+let postsSection = document.querySelector(".posts");
+postsSection.append(article);
+
+let firstArticle = postsSection.querySelector("article");
+let secondArticle = postsSection.children[1];
+postsSection.insertBefore(secondArticle, firstArticle);
+firstArticle = postsSection.children[0];
+secondArticle = postsSection.children[1];
+postsSection.insertBefore(firstArticle, secondArticle);
